8acd72ee87a10e5018e4d6cda76031a6e51c300d
oscar <oscarg3002@gmail.com>
2023-07-09 18:10:59 -0400

    fix

--- LabAsgmt_7.c
+++ LabAsgmt_7.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n, int swaps[]) {
-    int i, j, temp;
+void bubbleSort(int arr[], int n) {
+    int i, j, temp, swaps;
+    
     for (i = 0; i < n - 1; i++) {
+        swaps = 0;
+        
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                swaps[j]++;
+                swaps++;
             }
         }
+        
+        printf("Swaps needed for index %d: %d\n", i, swaps);
     }
 }
 
 int main() {
     int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int swaps[n];
-    for (int i = 0; i < n; i++) {
-        swaps[i] = 0;
-    }
-    
-    bubbleSort(arr, n, swaps);
-    
-    printf("Index\tSwaps\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\n", i, swaps[i]);
-    }
-    
+
+    bubbleSort(arr, n);
+
     return 0;
 }
